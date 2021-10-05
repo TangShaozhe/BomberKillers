7d79acf6de61a880313cdfd8b1b8551cf54fa7cb
Tang Shaozhe <qs4oz8@inf.elte.hu>
2021-10-05 10:48:09 +0200

    define the coordinators

--- source/object.py
+++ source/object.py
@@ -24,6 +24,12 @@ class Player(pg.sprite.Sprite):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
 
+    #define the coordinators
+    def zuobiaox(self):
+        return self.x
+    def zuobiaoy(self):
+        return self.y
+
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
@@ -53,12 +59,24 @@ class Box(pg.sprite.Sprite):
         self.rect.y = y * TILESIZE
 
 class Bomb(pg.sprite.Sprite):
-    def __init__(self,game,x,y,explode):
-        self.group = game.all_sprites, game.bombs
-        pg.sprite.Sprite.__init__(self,self.groups)
-        self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image = game.bomb_img
-        self.explode = explode
-        self.rect = self.image.get_rect()
-
+    def __init__(self,x,y,screen,ditu):
+        self.ditu=ditu
+        self.screen=screen
+        self.image=pg.image.load("image/bomb.png")
+        self.baozhaxiaoguo=pg.image.load("image/fire.png")
+        self.time=200#爆炸time
+        self.x=x
+        self.y=y
+        self.fanwei=2
+        self.left=-self.fanwei
+        self.right=self.fanwei
+        self.top=-self.fanwei
+        self.down=self.fanwei
+        #32x32
+    def update(self):
+        self.time-=1
+        if self.time<=70:
+            self.screen.blit(self.baozhaxiaoguo,(self.x*32,self.y*32))
+            
+        else:
+            self.screen.blit(self.image,(self.x*32,self.y*32))
