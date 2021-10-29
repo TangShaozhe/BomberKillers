22349815848685ffa18355531144217e27800a8f
Tang Shaozhe <qs4oz8@inf.elte.hu>
2021-10-29 06:01:34 +0200

    load images and data items

--- source/gameFunction.py
+++ source/gameFunction.py
@@ -31,7 +31,31 @@ class Game:
         #self.player_play=Player(self,3,21,self.screen)
         self.x=0
         #32x24
-
+def load_data(self):
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder,'image')
+        self.wall_img = pg.image.load(path.join(img_folder,WALL_IMAGE)).convert_alpha()
+        self.background_img = pg.image.load(path.join(img_folder,BACKGROUND_IMAGE)).convert_alpha()
+        self.box_img = pg.image.load(path.join(img_folder,BOX_IMAGE)).convert_alpha()
+        self.player_img_one = pg.image.load(path.join(img_folder,PLAYER_IMAGE_ONE)).convert_alpha()
+        self.player_img_two = pg.image.load(path.join(img_folder,PLAYER_IMAGE_TWO)).convert_alpha()
+        self.bomb_img = pg.image.load(path.join(img_folder,BOMB_IMG)).convert_alpha()
+        self.monster_bomb_img = pg.image.load(path.join(img_folder,MONSTER_BOMB_IMG)).convert_alpha()
+        self.fire_img =pg.image.load(path.join(img_folder,FIRE_IMG)).convert_alpha()
+        self.props_img =pg.image.load(path.join(img_folder,PROPS_IMG)).convert_alpha()
+        self.yaoshui_img =pg.image.load(path.join(img_folder,YAOSHUI_IMG)).convert_alpha()
+        self.monster_img_one =pg.image.load(path.join(img_folder,MONSTER_IMG_ONE)).convert_alpha()
+        self.monster_img_two =pg.image.load(path.join(img_folder,MONSTER_IMG_TWO)).convert_alpha()
+        self.map_data = []
+        self.bomb=[]#玩家炸弹
+        self.daoju_list=[]# 道具
+        self.robot_list=[]# 机器人
+        self.robot_bomb=[]# 机器人炸弹
+        self.box_list=[]# 箱子
+        with open(path.join(game_folder, 'map/map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line[:-1])
+        print(self.map_data)
 
 
 
