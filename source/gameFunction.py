ef0c214c18609c0daaf063f9082bdeb747d6024e
Tang Shaozhe <qs4oz8@inf.elte.hu>
2021-10-29 06:08:39 +0200

    from the master

--- source/gameFunction.py
+++ source/gameFunction.py
@@ -31,7 +31,12 @@ class Game:
         #self.player_play=Player(self,3,21,self.screen)
         self.x=0
         #32x24
-def load_data(self):
+        
+        """
+        if pygame.mixer.music.get_busy() == False: #检查是否正在播放音乐
+            pygame.mixer.music.play() #开始播放音乐流
+        """
+    def load_data(self):
         game_folder = path.dirname(__file__)
         img_folder = path.join(game_folder,'image')
         self.wall_img = pg.image.load(path.join(img_folder,WALL_IMAGE)).convert_alpha()
@@ -56,8 +61,11 @@ def load_data(self):
             for line in f:
                 self.map_data.append(line[:-1])
         print(self.map_data)
+        
 
-def new(self):
+        
+
+    def new(self):
         # start a new game
         self.playing = True
         self.all_sprites = pg.sprite.Group()
@@ -95,7 +103,8 @@ def new(self):
     def update(self):
         # Game loop - update
         self.all_sprites.update()
-def events(self):
+
+    def events(self):
         # Game loop - events
         for event in pg.event.get():
         # check for closing the window
@@ -140,6 +149,97 @@ def events(self):
                             elif j.id == 2:
                                 self.player_play.fanwei += 1
                             self.daoju_list.remove(j)
+    def draw_grid(self):
+        for x in range(0,WIDTH,TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY,(x,0),(x,HEIGHT))
+        for y in range(0,HEIGHT,TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY,(0,y),(WIDTH,y))
+
+    def draw(self):
+        # Game loop - draw
+        # Draw / render
+        #self.screen.fill(DARKGREY)
+        self.screen.blit(self.background_img,[0,0])
+        if len(self.box_list)>0:#箱子
+            for i in self.box_list:
+                if self.map_data[i.y][i.x]=="4":
+                    a = i.suiji()
+                    if self.player_play.x == i.x and self.player_play.y == i.y:
+                        if a == 1:
+                            self.player_play.fanwei += 1
+                        elif a == 2:
+                            self.player_play.shuliang += 1
+                    else:
+                        if a == 1 or a == 2:
+                            self.daoju_list.append(Daoju(self,self.screen, i.x, i.y, a))
+                    self.box_list.remove(i)
+                else:
+                    i.update()
+        if len(self.bomb) > 0:#玩家炸弹
+            for i in self.bomb:
+                if i.time == 70:
+                    #self.map_data[i.y] = self.map_data[i.y][:i.x] + "." + self.map_data[i.y][i.x + 1:]
+                    self.bomb_shuliang-=1
+                if i.time == 0:
+                    for x, y in i.list():
+                        self.map_data[y] = self.map_data[y][:x] + "." + self.map_data[y][x + 1:]
+                else:
+                    if i.time == 70:# 时间
+                        for x,y in i.list():
+                            self.map_data[y]= self.map_data[y][:x] + "4" + self.map_data[y][x + 1:]
+                        if self.map_data[self.player_play.y][self.player_play.x] == "4":
+                            self.player_play.hurt()
+                        for j in self.robot_list:
+                            if self.map_data[j.y][j.x] == "4" and j.shanshuo_zhen !=1:
+                                j.hurt()
+                                if j.hp == 0:
+                                    a = j.suiji()
+                                    if self.player_play.x == j.x and self.player_play.y == j.y:
+                                        if a == 1:
+                                            self.player_play.fanwei += 1
+                                        elif a == 2:
+                                            self.player_play.shuliang += 1
+                                    else:
+                                        if a == 1 or a == 2:
+                                            self.daoju_list.append(Daoju(self,self.screen,j.x,j.y,a))
+                                    self.robot_list.remove(j)
+                    i.update()
+        if len(self.robot_bomb) > 0:#怪兽的炸弹
+            for i in self.robot_bomb:
+                if i.time == 70:
+                    self.map_data[i.y] = self.map_data[i.y][:i.x] + "." + self.map_data[i.y][i.x + 1:]
+                if i.time == 0:
+                    for x, y in i.list():
+                        self.map_data[y] = self.map_data[y][:x] + "." + self.map_data[y][x + 1:]
+                    self.robot_bomb.remove(i)
+                else:
+                    if i.time == 70:
+                        for x, y in i.list():
+                            self.map_data[y] = self.map_data[y][:x] + "4" + self.map_data[y][x + 1:]
+                        if self.map_data[self.player_play.y][self.player_play.x] == "4"  :
+                            self.player_play.hurt()
+                    i.update()
+        self.player_play.update()
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        # * after drawing everything , flip the display
+        if len(self.daoju_list)>0:
+            for a in self.daoju_list:
+                a.update()
+        if len(self.robot_list)>0:
+            for i in self.robot_list:
+                i.update(self.map_data)
+                if i.fangzhi_zhadan():
+                    self.robot_bomb.append(Bomb(self,i.x,i.y,self.screen,self.map_data,i.fanwei,"guaishou"))
+        pg.display.update()
+
+    def show_start_screen(self):
+        # Game start screen
+        pass
+    def show_go_screen(self):
+        # Game over screen
+        pass
+
 
 
 
